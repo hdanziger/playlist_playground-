823e6ce7b335a0ebd2dd59055e87eb9568e07fc3
github username <github email address>
2018-04-01 13:54:04 -0400

    spotify

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import store from './store.js'
+import store from './store.js';
 
 ReactDOM.render(
   <Provider store={store}>
